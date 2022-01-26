02dee806014cc9d61f2d4e468cd449af6efdd1d1
Dimple Yadav <96530370+yadavDimple@users.noreply.github.com>
2022-01-26 23:51:26 +0530

    Add files via upload

---
+++ Jan-26/Assignments/second.js
@@ -0,0 +1,8 @@
+var input = prompt(" please enter any character");
+
+if(input=="a" ||input=="e" || input=="i" || input=="o" || input=="u"){
+alert("It is a  vowel");
+}
+else{
+alert("It is a consonant");
+}
