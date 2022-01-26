627b2cb8a08afda1558e064fa7ffcf68ec9d3eb7
Dimple Yadav <96530370+yadavDimple@users.noreply.github.com>
2022-01-26 23:13:03 +0530

    Add files via upload

---
+++ Jan-25/Assignments/second.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=20;
+var c=30;
+if(a>b && a>c){
+console.log("the grater number is", a);
+}
+else if(b>a && b>c){
+console.log("the grater number is", b);
+}
+else{
+
+console.log("the grater number is", c);
+}
