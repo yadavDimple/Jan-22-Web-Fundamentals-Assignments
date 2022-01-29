22fbf04869c0e2b98172843f5f6b7facf051978e
Dimple Yadav <96530370+yadavDimple@users.noreply.github.com>
2022-01-29 22:50:37 +0530

    Update second.js

--- Jan-26/Assignments/second.js
+++ Jan-26/Assignments/second.js
@@ -1,3 +1,5 @@
+1 - way
+
 var input = prompt(" please enter any character");
 
 if(input=="a" ||input=="e" || input=="i" || input=="o" || input=="u"){
@@ -6,3 +8,23 @@ if(input=="a" ||input=="e" || input=="i" || input=="o" || input=="u"){
     else{
        alert("It is a consonant");
 }
+
+2 - way
+
+var input = prompt(" please enter any character");
+
+ if( input == "a" ){
+              alert("It is a  vowel");
+       }   else if (input == "e") {
+                  alert("It is a vowel");
+       }   else if (input == "i"){
+                   alert("It is a vowel");
+       }    else if (input == "o"){
+                  alert("It is a vowel");
+       }    else if (input == "u"){
+                  alert("It is a vowel");
+      }
+             else{
+                    alert("It is a consonant");
+}
+
