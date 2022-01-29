3454e2ee541e647fc57bf45d4e5ffff045a9cae0
Dimple Yadav <96530370+yadavDimple@users.noreply.github.com>
2022-01-29 10:34:04 +0530

    Update first.js

--- Jan-26/Assignments/first.js
+++ Jan-26/Assignments/first.js
@@ -7,12 +7,12 @@ num2=parseInt(num2);
 num3=parseInt(num3);
 
 if(num1 < num2 && num1 < num3){
-alert(num 1 is minimum)
+    alert(num 1 is minimum)
 }
-else if(num2 < num1  &&  num2 < num3){
-alert(num 2 is minimum);
+    else if(num2 < num1  &&  num2 < num3){
+       alert(num 2 is minimum);
 
 }
-else{
-alert(num 3 is minimum);
+     else{
+         alert(num 3 is minimum);
 }
