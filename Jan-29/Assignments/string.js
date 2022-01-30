9d6c947b0dc6f88a04812f2f6f2892ce63133320
Dimple Yadav <96530370+yadavDimple@users.noreply.github.com>
2022-01-30 16:26:01 +0530

    Update string.js

--- Jan-29/Assignments/string.js
+++ Jan-29/Assignments/string.js
@@ -7,7 +7,8 @@ var i;
         for ( i = 0; i < str.length; i++)
         {
             if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
-                    || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+                    || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'A' ||
+                      str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O' || str.charAt(i) == 'U' )
             {
                 count++;
             }
